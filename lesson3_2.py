d9f26e17e91be2a075a91eb132cc453d15c1f66c
littleraining <eatfood1113@gmail.com>
2023-03-18 12:06:20 +0800

    modify

--- lesson3_2.py
+++ lesson3_2.py
@@ -1,1 +1,16 @@
-print('hollo! python!')
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Hello Tkinter")
+
+        label = tk.Label(self, text="Hello World!")
+        label.pack(fill=tk.BOTH, expand=1, padx=100, pady=50)
+
+def main():
+    window = Window()
+    window.mainloop()
+    
+if __name__ == "__main__":
+    main()
